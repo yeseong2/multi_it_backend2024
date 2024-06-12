3383f302e7f5b8a39366827188a0a9ab4f01f8a3
dptjd324 <dptjd324@naver.com>
2024-06-12 10:17:00 +0900

    최초작업입니다.

---
+++ src/main/java/com/example/multi_it_backend2024/MultiItBackend2024Application.java
@@ -0,0 +1,13 @@
+package com.example.multi_it_backend2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiItBackend2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiItBackend2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multi_it_backend2024/MultiItBackend2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multi_it_backend2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiItBackend2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
